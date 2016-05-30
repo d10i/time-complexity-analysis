09834a489144dca9df38ebe6ca4767d32bcc9338
dario-simonetti <dario.simonetti@askattest.com>
2016-05-30 23:33:15 +0100

    Upgraded to Java 8 given that it started working again...

--- code/run-no-agent.sh
+++ code/run-no-agent.sh
@@ -1,1 +1,1 @@
-/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java -jar ./time-complexity-analyser/target/time-complexity-analyser-fat.jar
+java -jar ./time-complexity-analyser/target/time-complexity-analyser-1.0-SNAPSHOT-fat.jar
--- code/run.sh
+++ code/run.sh
@@ -1,1 +1,1 @@
-/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java -javaagent:agent-instrumentation/target/agent-fat.jar -jar ./time-complexity-analyser/target/time-complexity-analyser-fat.jar
+java -javaagent:agent-instrumentation/target/agent-fat.jar -jar ./time-complexity-analyser/target/time-complexity-analyser-1.0-SNAPSHOT-fat.jar
