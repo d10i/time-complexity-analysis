09d33c51594d9887547fb0772ad0914657e30f69
dario-simonetti <dario.simonetti@askattest.com>
2017-04-22 20:22:43 +0100

    Remove unused files and use agent config

--- code/run.sh
+++ code/run.sh
@@ -1,1 +1,1 @@
-java -javaagent:agent-instrumentation/target/agent-fat.jar -jar ./time-complexity-analyser/target/time-complexity-analyser-1.0-SNAPSHOT-fat.jar
+java -javaagent:agent/target/agent-fat.jar=config.json -jar ./time-complexity-analyser/target/time-complexity-analyser-1.0-SNAPSHOT-fat.jar
